9ddb67762c281413ca2ad5fe43ada1c51a0babe3
oluseyi <oluseyi@cloudforgeacademy.ca>
2025-07-07 21:09:38 -0400

    updated readme.md

--- main.tf
+++ main.tf
@@ -1,9 +1,7 @@
 ###############ResourceGroup##################################
 resource "azurerm_resource_group" "rg" {
-  provider = azurerm.nonprddevsubscription
   name     = var.rgname
   location = var.rglocation
-
   tags = var.tags
 }
 
@@ -36,16 +34,16 @@ resource "azurerm_storage_account" "sw" {
 #################Staticweb-app #####################################
 resource "azurerm_storage_account_static_website" "sw" {
   storage_account_id = azurerm_storage_account.stw.id
-  error_404_document = "R3.8 Gore Prism Data Models Dictionary.html"
+  error_404_document = "Dictionary.html"
   index_document     = "index.html"
 }
 
 resource "azurerm_storage_account_network_rules" "sw" {
   storage_account_id = azurerm_storage_account.stw.id
   default_action     = "Deny"
-  ip_rules           = ["216.16.250.162", "99.209.20.226", "72.138.121.66"]
-  virtual_network_subnet_ids = [
-    data.azurerm_subnet.exisitng_subnet.id
-  ]
+  ip_rules           = ["216.16.250.162"]
+#   virtual_network_subnet_ids = [
+#     data.azurerm_subnet.exisitng_subnet.id
+#   ]
   bypass = ["AzureServices"]
 }
